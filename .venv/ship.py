edb131aaf61771ca010bfe56b49266d33096803d
Анна Михайлова <anmikhailova1308@gmail.com>
2025-05-16 15:26:44 +0300

    first lesson

---
+++ .venv/ship.py
@@ -0,0 +1,37 @@
+import pygame
+class Ship():
+    def __init__(self, ai_settings, screen):
+        """Инициализирует корабль и задает его начальную позицию."""
+        self.screen = screen
+        self.ai_settings = ai_settings
+
+        # Загрузка изображения корабля и получение прямоугольника.
+        self.image = pygame.image.load('images/ship.png')
+        self.rect = self.image.get_rect()
+        self.screen_rect = screen.get_rect()
+
+        # Каждый новый корабль появляется у нижнего края экрана.
+        self.rect.centerx = self.screen_rect.centerx
+        self.rect.bottom = self.screen_rect.bottom
+
+        # Сохранение вещественной координаты центра корабля.
+        self.center = float(self.rect.centerx)
+
+        # Флаг перемещения
+        self.moving_right = False
+        self.moving_left = False
+
+    def update(self):
+        """Обновляет позицию корабля с учетом флагов."""
+        # Обновляется атрибут center, не rect.
+        if self.moving_right:
+            self.center += self.ai_settings.ship_speed_factor
+        if self.moving_left:
+            self.center -= self.ai_settings.ship_speed_factor
+
+        # Обновление атрибута rect на основании self.center.
+        self.rect.centerx = self.center
+
+    def blitme(self):
+        """Рисует корабль в текущей позиции."""
+        self.screen.blit(self.image, self.rect)
